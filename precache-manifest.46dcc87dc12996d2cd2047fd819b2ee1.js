539d567dabfa823ff1b44c0f114793633819dc29
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-03-10 17:43:49 +0200

    Updates

---
+++ precache-manifest.46dcc87dc12996d2cd2047fd819b2ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee96c1e6e3460d388047675916c09f38",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b45039d030097f01c2a8",
+    "url": "/goit-react-hw-01-components/static/css/main.c1e3444a.chunk.css"
+  },
+  {
+    "revision": "00087a74acc0cb44d5cd",
+    "url": "/goit-react-hw-01-components/static/js/2.c8eed75f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.c8eed75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45039d030097f01c2a8",
+    "url": "/goit-react-hw-01-components/static/js/main.e71a408e.chunk.js"
+  },
+  {
+    "revision": "5e28ef9210cdb0f50653",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.47b9d3ea.js"
+  }
+]);
